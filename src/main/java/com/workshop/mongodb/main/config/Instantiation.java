4b8096e482c7f49d7864cfcb82d4df573110c320
n30config <mu-silva@outlook.com>
2021-01-21 12:41:13 -0300

    Configuration Instantiation

---
+++ src/main/java/com/workshop/mongodb/main/config/Instantiation.java
@@ -0,0 +1,33 @@
+package com.workshop.mongodb.main.config;
+
+import com.workshop.mongodb.main.entities.User;
+import com.workshop.mongodb.main.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TimeZone;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userReposiroty;
+
+
+    @Override
+    public void run(String... arg0) throws Exception {
+
+        userReposiroty.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+    }
+
+}
